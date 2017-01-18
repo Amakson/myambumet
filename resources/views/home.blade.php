bb05bdc01999f8ebf0c76105b0c7995c4cba2785
amakson <anthonymakson@gmail.com>
2017-01-18 13:06:39 +0000

    onwards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
+<main class="container">
                 <div class="panel-body">
                     You are logged in Enjoy!
                 </div>
-                <div class="panel-body">
-                    Medic
-                </div>
+    <div class="container-fluid">
+        <div class="jumbotron">
+            <h1>Dashboard</h1>
+        </div>   
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+
             </div>
         </div>
+        </div>
     </div>
-</div>
+</main>
 @endsection
